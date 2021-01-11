f7763bebe89fb9845b8c573a01e1812fda25a004
nesadrian <adrian.nesvik@lyse.net>
2021-01-11 17:30:46 +0100

    Add secure store compatability handling

--- components/PasswordsPage.js
+++ components/PasswordsPage.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import * as SecureStore from 'expo-secure-store';
+import ErrorPage from './ErrorPage'
 
 export default function PasswordsPage() {
+    const [secureStoreAvailable, setSecureStoreAvailable] = useState()
+
+    useEffect(() => {
+        (async () => {
+            const secureStoreAvailability = await SecureStore.isAvailableAsync()
+            setSecureStoreAvailable(secureStoreAvailability)
+        })();
+    }, [])
+
+    if(!secureStoreAvailable) {
+        return <ErrorPage message="Device not compatible" />
+    }
+
     return (
         <View style={styles.container}>
             <Text>Password Page</Text>
         </View>
     );
 }
-
 
 const styles = StyleSheet.create({
     container: {
