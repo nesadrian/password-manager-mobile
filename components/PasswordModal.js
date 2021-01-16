6561ea59e254e38e69f2789c314c18de159b9794
nesadrian <adrian.nesvik@lyse.net>
2021-01-16 18:06:12 +0100

    Add password modal styling

--- components/PasswordModal.js
+++ components/PasswordModal.js
@@ -12,8 +12,8 @@ export default function PasswordModal({ name, password }) {
       visible={true}
     >
       <View style={styles.popup}>
-          <Text>{name}</Text>
-          <Text>{showPassword ? password : '•'.repeat(password.length)}</Text>
+          <Text style={styles.name}>{name}</Text>
+          <Text style={showPassword ? styles.password : styles.passwordHidden}>{showPassword ? password : '•'.repeat(password.length)}</Text>
           <Button title={(showPassword ? 'Hide' : 'Show')} onPress={() => setShowPassword(!showPassword)} />
       </View>
     </Modal>
@@ -41,5 +41,20 @@ const styles = StyleSheet.create({
       borderColor: 'black',
       borderWidth: 1,
       borderRadius: 5,
+  },
+  name: {
+    fontWeight: '500',
+    fontSize: 17.5,
+  },
+  passwordHidden: {
+    fontWeight: '300',
+    fontSize: 45,
+    letterSpacing: -5,
+    margin: 10
+  },
+  password: {
+    fontWeight: '300',
+    fontSize: 30,
+    margin: 19,
   }
 });
